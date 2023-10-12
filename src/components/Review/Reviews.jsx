07f8bac7db332aa06e0a485480453c84cdd1f8eb
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2023-10-12 20:00:56 +0200

    first commit

--- src/components/Review/Reviews.jsx
+++ src/components/Review/Reviews.jsx
@@ -13,15 +13,15 @@ import Aos from "aos";
 
 const data = [
   {
-    text: "Nagyon elégedettek vagyunk a [Könyvelőiroda neve] szolgáltatásaival. Az elmúlt évek során folyamatosan segítettek nekünk az adózási és pénzügyi kérdésekben. A könyvelőink mindig elérhetők és segítőkészek voltak, és rendkívül alaposak a munkájukban. Az adóbevallásaink mindig időben és hibátlanul készültek el, és ezáltal pénzügyi terveink is zökkenőmentesen haladtak. Bízunk a [Könyvelőiroda neve] szolgáltatásaiban, és szívesen ajánljuk másoknak is.",
+    text: "Nagyon elégedettek vagyunk a City Tax szolgáltatásaival. Az elmúlt évek során folyamatosan segítettek nekünk az adózási és pénzügyi kérdésekben. A könyvelőik mindig elérhetők és segítőkészek voltak, és rendkívül alaposak a munkájukban. Az adóbevallásaink mindig időben és hibátlanul készültek el, és ezáltal pénzügyi terveink is zökkenőmentesen haladtak. Bízunk a City Tax szolgáltatásaiban, és szívesen ajánljuk másoknak is.",
     name: "Kovács Péter",
   },
   {
-    text: "Már évek óta az [Könyvelőiroda neve] ügyfelei vagyunk, és soha nem voltunk csalódottak. A csapat mindig hozzáértő és profi, és mindig kéznél vannak, amikor szükség van rájuk. A könyvelési és adózási tanácsaik nagyon hasznosak voltak a vállalkozásunk fejlesztésében, és köszönhetően nekik sikerült optimalizálnunk az adóterheinket. Nagyon köszönjük a [Könyvelőiroda neve] csapatnak a kiváló munkát!",
+    text: "Már évek óta a City Tax ügyfelei vagyunk, és soha nem voltunk csalódottak. A csapat mindig hozzáértő és profi, és mindig kéznél vannak, amikor szükség van rájuk. A könyvelési és adózási tanácsaik nagyon hasznosak voltak a vállalkozásunk fejlesztésében, és köszönhetően nekik sikerült optimalizálnunk az adóterheinket. Nagyon köszönjük!",
     name: "Nagy Andrea",
   },
   {
-    text: "A [Könyvelőiroda neve] szolgáltatásai valóban kiválóak. Amikor először kerestük őket, az üzletünk pénzügyi rendszere kaotikus volt, és szükségünk volt egy olyan könyvelőre, aki segít rendszerezni és optimalizálni. A [Könyvelőiroda neve] csapata nem csak a könyvelési munkát végezte el kitűnően, hanem tanácsokkal is ellátott minket, hogy hogyan növelhetjük a nyereségünket és minimalizálhatjuk az adóterheinket. Nagyon hálásak vagyunk nekik, és nem tudnánk elképzelni a vállalkozásunkat nélkülük.",
+    text: "A City Tax szolgáltatásai valóban kiválóak. Amikor először kerestük őket, az üzletünk pénzügyi rendszere kaotikus volt, és szükségünk volt egy olyan könyvelőre, aki segít rendszerezni és optimalizálni. A City Tax csapata nem csak a könyvelési munkát végezte el kitűnően, hanem tanácsokkal is ellátott minket, hogy hogyan növelhetjük a nyereségünket és minimalizálhatjuk az adóterheinket. Nagyon hálásak vagyunk nekik, és nem tudnánk elképzelni a vállalkozásunkat nélkülük.",
     name: "Horváth Gábor",
   },
 ];
