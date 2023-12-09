8ce70e70fd9ab12482135aee76c079487406002b
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2023-12-09 14:54:51 +0100

    cache

--- src/components/NavTop/LangugaModule.jsx
+++ src/components/NavTop/LangugaModule.jsx
@@ -1,15 +1,12 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-// import Image from "next/image";
 import Link from "next/link";
 import style from "@/components/NavTop/NavTop.module.css";
 import { usePathname } from "next/navigation";
 import useLanguagePathHandler from "../../app/hooks/languageHandler";
 
-//import hu from "../../../public/assets/icons/hu.png";
-//import de from "../../../public/assets/icons/de.png";
-//import en from "../../../public/assets/icons/en.png";
+
 
 const LangugaModule = () => {
   const { langHandlerEn, langHandlerHu, langHandlerDe } =
@@ -52,23 +49,3 @@ const LangugaModule = () => {
 
 export default LangugaModule;
 
-//const LangugaModule = ({langHandlerEn , langHandlerDe, langHandlerHu }) => {
-//  return (
-//    <><li >
-//    <Link href={langHandlerEn} className={style.socialIcon}>
-//    <Image className={style.imgIcon} alt='flag english' src={en} /></Link>
-//  </li>
-//  <li>
-//    <Link href={langHandlerDe} className={style.socialIcon}>
-//      <Image className={style.imgIcon} alt='flag german' src={de} />
-//    </Link>
-//  </li>
-//  <li>
-//    <Link href={langHandlerHu} className={style.socialIcon}>
-//    <Image className={style.imgIcon} alt='flag hungarian' src={hu} />
-//    </Link>
-//  </li></>
-//  )
-//}
-//
-//export default LangugaModule
